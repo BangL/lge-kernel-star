63505d16f0d486b026ac5004b2091656bc0f451b
BangL <henno.rickowski@googlemail.com>
2012-05-25 21:34:38 +0200

    newer settings

--- include/linux/skynet.h
+++ include/linux/skynet.h
@@ -18,13 +18,13 @@
 #define SKYNET_BOOT_CLOCK 1100000
 
 #define SKYNET_VOLT_1 775
-#define SKYNET_VOLT_2 800
-#define SKYNET_VOLT_3 825
-#define SKYNET_VOLT_4 900
-#define SKYNET_VOLT_5 1000
+#define SKYNET_VOLT_2 775
+#define SKYNET_VOLT_3 800
+#define SKYNET_VOLT_4 850
+#define SKYNET_VOLT_5 950
 #define SKYNET_VOLT_6 1075
-#define SKYNET_VOLT_7 1250
-#define SKYNET_VOLT_8 1350
+#define SKYNET_VOLT_7 1175
+#define SKYNET_VOLT_8 1275
 
 #define SKYNET_CLOCK_1 216000
 #define SKYNET_CLOCK_2 503000
@@ -35,14 +35,16 @@
 #define SKYNET_CLOCK_7 1408000
 #define SKYNET_CLOCK_8 1504000
 
+//#define max_screenoff_frequency 503000
+
 #define USE_FAKE_SHMOO
 
 // /mm
-#define dirty_background_ratio_default 2
-#define vm_dirty_ratio_default 4
+#define vm_dirty_ratio_default 50
+#define dirty_background_ratio_default 30
 #define inactive_file_ratio_default 20
-#define vfs_cache_pressure 25
-#define default_swappiness 100
+#define vfs_cache_pressure 100000
+#define default_swappiness 99
 #define VM_MAX_READAHEAD 32
 #define VM_MIN_READAHEAD 16
 
@@ -55,4 +57,4 @@
 #define sysctl_sched_wakeup_granularity_default 1000000UL
 #define normalized_sysctl_sched_wakeup_granularity_default 1000000UL
 #define CFS_BOOST
-#define CFS_BOOST_NICE -19
+#define CFS_BOOST_NICE -17
