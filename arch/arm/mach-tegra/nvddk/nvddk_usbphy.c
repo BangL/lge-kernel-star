91172111c8cc32d5d056139de6bfe07729f91818
BangL <henno.rickowski@googlemail.com>
2012-08-03 16:31:39 +0200

    Revert "Me Genius. Stupid typo"
    This reverts commit f0191c333b967d7d64202dcc50727e72a167c481.

--- arch/arm/mach-tegra/nvddk/nvddk_usbphy.c
+++ arch/arm/mach-tegra/nvddk/nvddk_usbphy.c
@@ -768,7 +768,7 @@ NvDdkUsbPhyPowerDown(
         /* Wait till Vbus is turned off */
         do
         {
-            NvOsSleepUS_interruptible(1000);
+            NvOsWaitUS_interruptible(1000);
             TimeOut -= 1000;
             e = hUsbPhy->Ioctl(hUsbPhy,
                     NvDdkUsbPhyIoctlType_VBusStatus,
